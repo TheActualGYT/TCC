3bbda7a30583e70bef6d344ff1bcb4a76dbe0b9a
GYT <135925814+TheActualGYT@users.noreply.github.com>
2024-12-04 19:21:44 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+<script>
+  function showMainPage() {
+    document.getElementById('main-page').style.display = 'block';
+    document.getElementById('leaderboard').style.display = 'none';
+  }
 
+  function showLeaderboard() {
+    document.getElementById('main-page').style.display = 'none';
+    document.getElementById('leaderboard').style.display = 'block';
+  }
+</script>
