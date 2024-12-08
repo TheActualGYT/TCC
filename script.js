8eb72c54b26b81f8b6f908f20f192e93ab9d9ad5
GYT <135925814+TheActualGYT@users.noreply.github.com>
2024-12-08 16:17:33 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function switchTab(buttonId, contentId) {
-  const contentIds = ["main-page-content", "leaderboard-content", "changelog-content", "sink-content", "nightshade-content", "timor-content", "the-moon-below-content", "lucid-chaos-content", "butiti-iii-content", "confusion-content", "stalemate-redux-content", "killbot-content", "omicron-content", "polygonal-paradox-content", "neurostasis-content", "black-blizzard-content", "berserk-content", "hyper-paradox-content"];
-  const buttonIds = ["main-page-btn", "leaderboard-btn", "changelog-btn", "sink-btn", "nightshade-btn", "timor-btn", "the-moon-below-btn", "lucid-chaos-btn", "butiti-iii-btn", "confusion-btn", "stalemate-redux-btn", "killbot-btn", "omicron-btn", "polygonal-paradox-btn", "neurostasis-btn", "black-blizzard-btn", "berserk-btn", "hyper-paradox-btn"];
+  const contentIds = ["main-page-content", "leaderboard-content", "changelog-content", "sink-content", "nightshade-content", "timor-content", "the-moon-below-content", "lucid-chaos-content", "butiti-iii-content", "confusion-content", "stalemate-redux-content", "killbot-content", "omicron-content", "polygonal-paradox-content", "neurostasis-content", "black-blizzard-content", "berserk-content", "hyper-paradox-content", "dmg-ctrl-content"];
+  const buttonIds = ["main-page-btn", "leaderboard-btn", "changelog-btn", "sink-btn", "nightshade-btn", "timor-btn", "the-moon-below-btn", "lucid-chaos-btn", "butiti-iii-btn", "confusion-btn", "stalemate-redux-btn", "killbot-btn", "omicron-btn", "polygonal-paradox-btn", "neurostasis-btn", "black-blizzard-btn", "berserk-btn", "hyper-paradox-btn", "dmg-ctrl-btn"];
 
   // Update content visibility
   contentIds.forEach(id => document.getElementById(id).classList.remove("active"));
@@ -30,3 +30,4 @@ document.getElementById("neurostasis-btn").addEventListener("click", () => switc
 document.getElementById("black-blizzard-btn").addEventListener("click", () => switchTab("black-blizzard-btn", "black-blizzard-content"));
 document.getElementById("berserk-btn").addEventListener("click", () => switchTab("berserk-btn", "berserk-content"));
 document.getElementById("hyper-paradox-btn").addEventListener("click", () => switchTab("hyper-paradox-btn", "hyper-paradox-content"));
+document.getElementById("dmg-ctrl-btn").addEventListener("click", () => switchTab("dmg-ctrl-btn", "dmg-ctrl-content"));
